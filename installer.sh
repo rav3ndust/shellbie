dbc41f7dc7adfe980faf4e6798d2c7cee724ed22
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-03-30 04:38:11 -0500

    changed some stuff

--- installer.sh
+++ installer.sh
@@ -3,42 +3,45 @@
 ######################################
 # vars
 ######################################
-first="Would you like to install Assistant dependencies?" 
-installing="Installing needed stuff for Assistant..."
-done_installing="Needed stuff for Assistant installed." 
+first=$(echo "Would you like to install Assistant dependencies?") 
+installing=$(echo "Installing needed stuff for Assistant...")
+done_installing=$(echo "Needed stuff for Assistant installed.") 
 updater=$(sudo apt update -y) 
 materials=$(sudo apt install links2 python3 cowsay libnotify-bin -y)
 sleep1=$(sleep 1) 
 notify1=$(notify-send "Little Linux Assistant" "Assistant dependencies installed.")
 notify2=$(notify-send "Little Linux Assistant" "Dependencies installation cancelled.")
-cancelling="Okay. Not installing." 
-cancelling2="Invalid key pressed. Exiting."
+cancelling=$(echo "Okay. Not installing.") 
+cancelling2=$(echo "Invalid key pressed. Exiting.")
+all_ops=$(echo "All operations finished.") 
 ######################################
 # functions
 ######################################
 function installingStuff() {
-	echo($installing) 
+	$installing 
 	$updater && $materials
-	echo($done_installing) & $notify1 
+	$done_installing & $notify1 
 	$sleep1
+	$all_ops
+	$sleep1 
 	echo "Exiting..." 
 	$sleep1
 	exit
 }
 function xxExit() {
-	echo($cancelling) & $notify2
+	$cancelling & $notify2
 	$sleep1
 	exit
 }
-function inValid() {
-	echo($cancelling2) & $notify2 
+function invalid_Input() {
+	$cancelling2 & $notify2 
 	$sleep1
 	exit 
 }
 ######################
 # script begins:
 ######################
-echo($first) 
+$first 
 read -p "Type 'Yes' if you want to install, or 'No' if you don't: " installYN
 if [[ $installYN == "Yes" ]] || [[ $installYN == "yes" ]]
 then
@@ -47,5 +50,5 @@ elif [[ $installYN == "No" ]] || [[ $installYN == "no" ]]
 then 
 	xxExit
 else 
-	inValid
+	invalid_Input
 fi 
