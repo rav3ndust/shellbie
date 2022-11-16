44a019722c6ddba4e3952d5b75fe2e857aaadedd
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-11-16 04:02:16 -0600

    Update weather.py

--- library/weather.py
+++ library/weather.py
@@ -2,7 +2,9 @@
     It is meant to be imported into the mainMenu and called when needed.'''
 import os as sh 
 from sources import * 
-sleep0 = 'sleep 1' 
+# - - - functions - - - 
+def sleep (seconds):
+    sh.system(f'sleep {seconds}')
 def getWeather():
     print(f'Getting the weather for {zip_code}...')
     sh.system(sleep0)
