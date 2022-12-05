f3da39658c93593959c09501121e59725d544a01
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-12-05 00:57:55 -0600

    Create shellbie.py

---
+++ shellbie.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+from gtts import gTTS as speech
+# - - - Variables - - -
+asst = "Shellbie"
+options = [
+        "Get Weather",
+        "Get News",
+        "Update the System",
+        "Play Some Music",
+        "Shut Down or Reboot Computer"
+    ]
+# - - - Functions - - -
+def speak(content):
+    tts = speech(text=content, lang="en")
+    tts.save("output.mp3")
+    os.system("mpg123 -q output.mp3")
+    # clean up the file
+    os.system("rm output.mp3")
+def give_options():
+    v1 = "Here are some options for you to choose from."
+    v2 = "Please make a selection."
+    speak(v1)
+    for option in options:
+        speak(option)
+    speak(v2)
+def greet():
+    system_user = os.popen("echo $USER").read()
+    current_date = os.popen("date").read()
+    system_hostname = os.popen("echo $HOSTNAME").read()
+    speak(f'Hi, {system_user}! My name is {asst}, running on {system_hostname}.')
+    speak(f'The current date and time is {current_date}.')
+    speak("What can I do for you today?")
+
+# program start
+greet()
+give_options()
